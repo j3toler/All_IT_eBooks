25d96e41696cfd84955086547b7aa46d17ff2739
Jay Toler <jay.toler@hotmail.com>
2019-12-15 15:53:31 -0500

    Added some local variables for future use. Additionally, created a logger.

--- main.py
+++ main.py
@@ -23,6 +23,35 @@ except ImportError, e:
     exit(0)
 
 
+#################### Set some Values ####################
+pattern_PDF = "^http://file.allitebooks.com/.*\.pdf"
+categories = {"Quit": None,
+    "Web-Development": 190,
+    "Programming": 173,
+    "Datebases": 91,
+    "Graphics-Design": 32,
+    "Operating-Systems": 76,
+    "Networking-Cloud-Computing": 73,
+    "Administration": 24,
+    "Certification": 18,
+    "Computers-Technology": 26,
+    "Enterprise": 18,
+    "Game-Programming": 33,
+    "Hardware": 35,
+    "Marketing-SEO": 7,
+    "Security": 19,
+    "Software": 40,
+    "All": None}
+small_break = "-----------------------------------"
+big_break = "==================================="
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename = ".\\run.log",
+                    level = logging.DEBUG,
+                    format = LOG_FORMAT,
+                    filemode = 'w')
+logger = logging.getLogger()
+
+
 #################### Define some Colors ####################
 def colorRED(text):
     """
