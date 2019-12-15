5dba98b708e1132ab04b97374f9f67cc2b58533c
Jay Toler <jay.toler@hotmail.com>
2019-12-15 15:48:16 -0500

    Included Import statements in the beginning of main.py

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ Created on Sun Dec 15 15:38:10 2019
 @author: Jay
 """
 
+
+#################### Do some Imports ####################
+try:
+    from os import path, makedirs, system
+    from bs4 import BeautifulSoup as bs
+    from clint.textui import colored
+    from collections import deque
+    from threading import Thread
+    from Queue import Queue
+    import unicodedata
+    import requests
+    import logging
+    import re
+except ImportError, e:
+    print "Could not import all modules: %s" % str(e)
+    exit(0)
+
 def main():
     """
        MISSING DESCRIPTION
