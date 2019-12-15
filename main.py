96764374601af43da42bd9bc0e2c6d51bba43e58
Jay Toler <jay.toler@hotmail.com>
2019-12-15 15:45:45 -0500

    Initial commit of empty main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 15:38:10 2019
+
+@author: Jay
+"""
+
+def main():
+    """
+       MISSING DESCRIPTION
+    """
+    pass
+
+if __name__ == "__main__":
+    pass
